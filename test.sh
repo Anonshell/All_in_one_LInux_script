d87e6f6893e74916925f1eb688e4712941ec8878
Ammar Aslam <79783076+Anonshell@users.noreply.github.com>
2021-08-06 10:47:59 +0600

    second update

--- test.sh
+++ test.sh
@@ -1,50 +1,128 @@
 #! /bin/bash
+
 banner(){
+
 echo
+
 echo
+
 echo                                                             "############################################"
+
 echo                                                             "#                                          #"
+
 echo                                                             "#           ALL IN ONE  SCRIPT             #"
+
 echo                                                             "#             @author-->Ammar<--           #"
+
 echo                                                             "#                                          #"
+
 echo                                                             "############################################"
+
 }
+
+
 
 message(){
+
 echo "Press 1 to check ip"
+
 echo "Press 2 to check  MAC"
+
 echo "Press 3 to check ping"
+
 echo "Press 0 to Exit"
+
 }
+
+
+
+checkroot(){
+
+
+
+if [ `whoami` != 'root' ]
+
+ then
+
+    echo "You must be root to do this."
+
+    exit 
+
+ fi
+
+    }
+
+
+
+
+
+
+
 main(){
+
+checkroot
+
+
+
 read -p "Select your option :" option
 
+
+
 if  [ $option -eq 1 ]
+
  then  
+
     echo "${red}IP = " $(hostname -I)
+
 elif [ $option -eq 2 ]
+
  then 
+
       echo "MAC address = " $(ip link show eth0 | grep link/ether |awk '{print $2}')
+
 elif [ $option -eq 3 ]
+
  then 
+
      echo -n "Enter domain or Ip : "
+
       read pg
+
 echo -n "Enter packet  value : "
+
 read pk
+
 ping -c$pk $pg
 
+
+
 elif [ $option -eq 0 ]
+
  then
+
         exit
+
 else  
+
  echo "Wrong option!"
+
 fi
+
 }
+
+
+
 
 
 while true
+
 do
+
 banner
+
 message
+
 main
+
 done
+
